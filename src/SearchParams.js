b80c2e41b1834f524dd96ae4f75995163f7bed31
Shan <shan.mi.fanfan@gmail.com>
2020-06-03 23:31:33 +0200

    Custom hooks
    Theoretically understand that, but needs some time to write such kind of
    thing out.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
-  // if (false) {
-  //   const [location, setLocation] = useState("Seattle, WA");
-  // }
   const [location, setLocation] = useState("Seattle, WA"); // That is the default state, and use destructuring here to get two items from that array we got from useState.
   // This is a hook! All hooks begin with 'use'
   // location: current state; setLocation: an update function for that state - get back a hook always get back an array following this pattern.
   // use state create a hook
+  const [breeds, setBreeds] = useState([]); //breeds will be an array
+  // const [animal, setAnimal] = useState("dog");
+  // const [breed, setBreed] = useState("");
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
 
   return (
     <div className="search-params">
@@ -21,6 +25,8 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
