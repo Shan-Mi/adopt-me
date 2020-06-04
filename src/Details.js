e681573aa3641a90bb26ff48e52b21d7203fb020
Shan <shan.mi.fanfan@gmail.com>
2020-06-04 15:38:17 +0200

    ErrorBoundary added
    ErrorBoundary will only detect any errors which occur inside of it, aka
    as its children, so we have to go one level higher above the component
    that we want to check if any error happens inside of it.

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,12 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends React.Component {
   state = { loading: true };
   componentDidMount() {
+    throw new Error("lol");
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
         name: animal.name,
@@ -37,4 +39,15 @@ class Details extends React.Component {
   }
 }
 
-export default Details;
+// export default Details;
+
+// export default ErrorBoundary;
+// If you had something like Azure Monitor or Track JS or Century etc..
+// you will prob send that off to the tiered consolidated logging service.
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
---
+++ src/ErrorBoundary.js
@@ -0,0 +1,28 @@
+// mostly code from reactjs.org/docs/error-boundaries.html
+
+import React from "react";
+import { Link } from "@reach/router";
+
+class ErrorBoundary extends React.Component {
+  state = { hasError: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary caught en error", error, info);
+  }
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h1>
+          There was an error with this listing. <Link to="/">Click here</Link>{" "}
+          to go back to the home page or wait five seconds.
+        </h1>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
