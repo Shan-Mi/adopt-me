0f7f175eac5bf862b6cd7b02ca902f3c3e3837d3
Shan <shan.mi.fanfan@gmail.com>
2020-06-04 12:03:37 +0200

    Added Results.js
    Use async to fetch data from API, and rememeber to empty selected
    options after choosing new breed.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
@@ -8,14 +9,45 @@ const SearchParams = () => {
   // location: current state; setLocation: an update function for that state - get back a hook always get back an array following this pattern.
   // use state create a hook
   const [breeds, setBreeds] = useState([]); //breeds will be an array
-  // const [animal, setAnimal] = useState("dog");
-  // const [breed, setBreed] = useState("");
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  // Animal: label; dog: default state; ANIMALS: list of options to choose from
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals || []);
+  }
+
+  // handle data asynchronized
+  // run after the render happens
+  useEffect(() => {
+    // pet.breeds("dog").then(console.log, console.error); //return a promise
+
+    // This is to clear the input, aka the set of breeds after we choose new breed.
+    // After that we re-render the page with that new breed (if we choose a new breed)
+    setBreeds([]);
+    setBreed(""); // get it from useDropdown
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name); // destructuring breeds and turn property name to string
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]); // this is a list of dependencies, react is going to check if any one of these changed, rerun this effect after it renders; if location changes, it will not rerun this effect.
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -29,6 +61,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
