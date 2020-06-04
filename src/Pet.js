aca37e7bee9abd02b97f6bb23478ded7539cdc68
Shan <shan.mi.fanfan@gmail.com>
2020-06-04 13:14:53 +0200

    Added reach-router
    Use reach-router to automatically handle different routing mechanisms,
    and rememeber to use <link> instead of <a>.

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-export default function Pet({ name, animal, breed }) {
+export default function Pet({ name, animal, breed, media, location, id }) {
+  let hero = "http://placecorgi.com/300/300"; // Image placeholder
+  if (media.length) {
+    hero = media[0].small;
+  }
   return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   );
 }
