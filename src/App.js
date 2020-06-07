ccc83065299fc090d14cb6529325d5c14b34da18
Shan <shan.mi.fanfan@gmail.com>
2020-06-07 13:50:51 +0200

    Use Emotion to write inline-CSS directly
    install @emotion/babel-preset-css-prop, then we are allowed to insert
    css in JS.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 import ThemeContext from "./ThemeContext";
+import NavBar from "./NavBar";
 
 const App = () => {
   // set some default value
@@ -15,9 +16,7 @@ const App = () => {
     <React.StrictMode>
       <ThemeContext.Provider value={themeHook}>
         <div>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
+          <NavBar />
 
           <Router>
             <SearchParams path="/" />
