9e5b4b372688d1c47863924bbd60213be92a2e88
Shan <shan.mi.fanfan@gmail.com>
2020-06-07 15:54:45 +0200

    Code splitting
    Import {lazy, Suspense} from react to split code out to speed up loading
    current page, Suspense can be put on route and it will take over all
    lazy import.
    We should do the splitting out code for at least 30 kb plus, otherwise it won't be
    worth it.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { render } from "react-dom";
-import { Router } from "@reach/router";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
+import { Router, Link } from "@reach/router";
+// import SearchParams from "./SearchParams";
+// import Details from "./Details";
+// delete that, because we don't want Parcel to bundle that details right now.
 import ThemeContext from "./ThemeContext";
-import NavBar from "./NavBar";
+
+const Details = lazy(() => import("./Details"));
+// dynamically import, it is a js thing. it is a promise thing
+// Now Details is a placeholder comoponent that will not load this code until later when Details is actually tried to be rendered for the first time.
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   // set some default value
@@ -16,12 +21,15 @@ const App = () => {
     <React.StrictMode>
       <ThemeContext.Provider value={themeHook}>
         <div>
-          <NavBar />
-
-          <Router>
-            <SearchParams path="/" />
-            <Details path="/details/:id" />
-          </Router>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Suspense fallback={<h1>loading route...</h1>}>
+            <Router>
+              <SearchParams path="/" />
+              <Details path="/details/:id" />
+            </Router>
+          </Suspense>
         </div>
       </ThemeContext.Provider>
     </React.StrictMode>
